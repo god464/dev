35022762e5afc0750001279d591c4eac3165088e
god464 <god464@users.noreply.github.com>
2025-07-23 18:16:04 +0800

    fix: add codelldb lib

--- pkgs/codelldb/package.nix
+++ pkgs/codelldb/package.nix
@@ -1,17 +1,26 @@
 { stdenv, pkgs, ... }:
+let
+  codelldb_package = pkgs.vscode-extensions.vadimcn.vscode-lldb;
+  codelldb_path = "${codelldb_package}/share/vscode/extensions/vadimcn.vscode-lldb";
+in
 stdenv.mkDerivation {
   pname = "codelldb";
-  inherit (pkgs.vscode-extensions.vadimcn.vscode-lldb) version meta;
+  inherit (codelldb_package) version meta src;
 
-  src = ./.;
+  nativeBuildInputs = [ pkgs.autoPatchelfHook ];
 
   dontUnpack = true;
   dontBuild = true;
   dontConfigure = true;
 
   installPhase = ''
-    mkdir -p $out/bin
-    cp -r ${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/* $out/bin
+    runHook preInstall
+
+    mkdir -p $out/bin $out/bin
+    cp -r ${codelldb_path}/adapters/ $out/bin
+    cp -r ${codelldb_path}/lldb/lib $out/lib
     chmod +x $out/bin/codelldb
+
+    runHook postInstall
   '';
 }
